a5eb77322c2ad3f4dd8abd80223cfa239454b7df
ikramuddin21 <ikramuddin2122@gmail.com>
2022-05-20 12:27:45 +0600

    theme details added

--- src/components/ThemetaskDetails/ThemetaskDetails.js
+++ src/components/ThemetaskDetails/ThemetaskDetails.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import copy from '../../images/copy.svg';
 import share from '../../images/share.svg';
 import pinterest from '../../images/pinterest.svg';
+import './ThemetaskDetails.css';
 
 const ThemetaskDetails = () => {
     const { image_id } = useParams();
@@ -17,57 +18,60 @@ const ThemetaskDetails = () => {
             })
     }, [image_id]);
 
-    const {category:{name}} = themeDetails?.detail;
-    console.log(themeDetails?.detail?.category.name);
-
-    // const { preview, title, creation_ago, id, createdAt, extension, category:{name} } = themeDetails?.detail;
+    // const { preview, title, creation_ago, id, createdAt, extension } = themeDetails?.detail;
     return (
-        <div>
-            {/* <div>
-                <img src={`https://piktask.com/media_images/images/${preview}`} alt="" />
+        <div className="theme-details">
+            <div className="theme-details-left">
+                <img src={`https://piktask.com/media_images/images/${themeDetails?.detail?.preview}`} alt={themeDetails?.detail?.title} />
             </div>
 
-            <div>
-                <h3>{title}</h3>
-
-                <div>
-                    <span>{creation_ago}</span>
-                    <button><img src={share} alt="Share icon" /> Share</button>
-                    <button><img src={copy} alt="Copy icon" /> Copy Link</button>
-                    <button><img src={pinterest} alt="Pinterest icon" /> Save</button>
+            <div className="theme-details-right">
+                <h2 className="theme-details-title">{themeDetails?.detail?.title}</h2>
+                <div className="copy-share-area">
+                    <span>{themeDetails?.detail?.creation_ago}</span>
+                    <button className="theme-details-share">
+                        <img src={share} alt="Share icon" /> 
+                        <span>Share</span>
+                    </button>
+                    <button className="theme-details-copy">
+                        <img src={copy} alt="Copy icon" />
+                        <span>Copy Link</span>
+                    </button>
+                    <button className="theme-details-pinterest">
+                        <img src={pinterest} alt="Pinterest icon" />
+                        <span>Save</span>
+                    </button>
                 </div>
 
-                <div>
-                    <p><strong>Image ID: </strong>{id}</p>
-                    <p><strong>Created: </strong>{createdAt}</p>
-                </div>
-
-                <div>
-                    <p><strong>File Format: </strong>{extension}</p>
-                    <p><strong>Category: </strong>{name}</p>
-                </div>
-
-                <div>
-                    <p>
-                        <strong>Copyright Information: </strong>
-                        <span>Piktask</span>
-                    </p>
-
-                    <p>
-                        <strong>Scope of authorization: </strong>
-                        <span>Personal/Enterprise</span>
-                    </p>
-                </div>
-
-                <div>
-                    <img src={`https://piktask.com/media_images/${avatar}`} alt="" />
+                <div className="d-flex">
                     <div>
-                        <span>{username}</span>
-                        <span>{total_resources} Resources</span>
+                        <p><strong>Image ID: </strong>{themeDetails?.detail?.id}</p>
+                        <p><strong>File Format: </strong>{themeDetails?.detail?.extension}</p>
+                        <p>
+                            <strong>Copyright Information: </strong>
+                            <span>Piktask</span>
+                        </p>
+                    </div>
+
+                    <div>
+                        <p><strong>Created: </strong>{themeDetails?.detail?.createdAt}</p>
+                        <p><strong>Category: </strong>{themeDetails?.detail?.category?.name}</p>
+                        <p>
+                            <strong>Scope of authorization: </strong>
+                            <span>Personal/Enterprise</span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="user-info">
+                    <img src={`https://piktask.com/media_images/${themeDetails?.detail?.user?.avatar}`} alt="" />
+                    <div>
+                        <span>{themeDetails?.detail?.user?.username}</span>
+                        <span>{themeDetails?.detail?.user?.total_resources} Resources</span>
                     </div>
                     <button>Follow</button>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 };
