4f2a8ad92f6b14eda5608ada6061a7a7b18e052f
ikramuddin21 <ikramuddin2122@gmail.com>
2022-05-24 00:36:02 +0600

    offCanvas menu added

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import logInIcon from '../../images/logIn.svg';
+import menuIcon from '../../images/menu-icon.svg';
 import './Header.css';
 
 const Header = ({ setModal }) => {
 
     const [stickyHeader, setStickyHeader] = useState(false);
+    const [showMenuIcon, setShowMenuIcon] = useState(true);
 
     // handle sticky header
     const handleStickyHeader = () => {
@@ -20,38 +22,53 @@ const Header = ({ setModal }) => {
 
     window.addEventListener("scroll", handleStickyHeader);
 
+    // handle sell content button
+    const handleSellContentBtn = () => {
+        setModal(true);
+        setShowMenuIcon(true);
+    };
+
+    // handle login button
+    const handleLoginBtn = () => {
+        setModal(true);
+        setShowMenuIcon(true);
+    };
+
     return (
         <header className={stickyHeader ? "header active" : "header"}>
-            <nav>
+            <button className={!showMenuIcon ? "show-closeIcon" : "close-icon"} onClick={() => setShowMenuIcon(true)}>&times;</button>
+            <nav className={!showMenuIcon ? "show-nav" : ""}>
                 <div className="logo-area">
                     <Link to="/">
-                        <img src={logo} alt="Logo" />
+                        <img className="logo" src={logo} alt="Logo" onClick={() => setShowMenuIcon(true)} />
                     </Link>
+
+                    <img className={showMenuIcon ? "menu-icon" : "hide-menu"} src={menuIcon} alt="" onClick={() => setShowMenuIcon(false)} />
                 </div>
 
-                <ul className="list-items">
+                <ul className={!showMenuIcon ? "list-items show-header-list-items" : "list-items"}>
                     <li>
-                        <Link to="/category/business-card-mockup">Business Card Mockup</Link>
+                        <Link to="/category/business-card-mockup" onClick={() => setShowMenuIcon(true)}>Business Card Mockup</Link>
                     </li>
                     <li>
-                        <Link to="/category/text-effect">Text Effect</Link>
+                        <Link to="/category/text-effect" onClick={() => setShowMenuIcon(true)}>Text Effect</Link>
                     </li>
                     <li>
-                        <Link to="/category/social-media-banner">Social Media Banner</Link>
+                        <Link to="/category/social-media-banner" onClick={() => setShowMenuIcon(true)}>Social Media Banner</Link>
                     </li>
                     <li>
-                        <Link to="/category/game">Game</Link>
+                        <Link to="/category/game" onClick={() => setShowMenuIcon(true)}>Game</Link>
                     </li>
                 </ul>
 
-                <div className="header-right">
+                <div className={!showMenuIcon ? "header-right show-header-right" : "header-right"}>
                     <button
                         className="header-sell-content-btn"
-                        onClick={() => setModal(true)}
+                        onClick={handleSellContentBtn}
                     >Sell Your Content</button>
                     <button
                         className='log-in-btn'
-                        onClick={() => setModal(true)}
+                        onClick={handleLoginBtn}
                     ><img src={logInIcon} alt="" /> Log In</button>
 
                 </div>
